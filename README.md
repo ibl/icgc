897ddb61e1e70b14bbc72496a881258ff8b2e4d7
Jonas Almeida <jalmeida@mathbiol.org>
2014-05-30 17:02:00 -0500

    portal search added

--- README.md
+++ README.md
@@ -15,6 +15,39 @@ Experimenting with the possibility of having the [Web API](http://www.cbioportal
 cbio.\<cmd\>( callbackFunction , { argument<sub>i</sub> : value<sub>i</sub> , ... } );
 </i>
 
-Many thanks to [ICGC](http://www.mskcc.org/)'s [Portal](https://dcc.icgc.org/docs/) team - this is a great public service !
+Many thanks to [ICGC](http://www.icgc.org/)'s [Portal](https://dcc.icgc.org/docs/) team - this is a great public service !
 
 ---
+
+As detailed in ICGC (API documentation)[https://dcc.icgc.org/docs/], the operation of the portal web service is described by two elements, 1) a command and 2) a set of parameters, which are composed as a URL call:
+
+		<base URL> / <API version> / <COMMAND> ? <PARAMETERS>
+
+		for example, for the list of pathways involving P53 this would be
+
+		https://dcc.icgc.org:443/api/v1/genes/ENSG00000141510?field=pathways
+
+## .get
+
+The icgc.js client object mediates this exchange by processing an object where the <COOMAND> information is defined by a .cmd attribute and all other attributes describe the parameters, for example 
+
+		icgc.get({cmd:"genes/ENSG00000141510",field:"pathways"})
+
+		or illustrating the use of a filter
+
+		icgc.get({cmd:"genes/ENSG00000141510",filters:{"mutation":{"functionalImpact":{is:["High"]}},"donor":{"projectId":{is:["MALY-DE"]}}, "tumourStageAtDiagnosis":{"is":["III"]}}})
+
+## .portal
+
+A careful inspection of ICGC's (API documentation)[https://dcc.icgc.org/docs/] and the (example queries)[https://dcc.icgc.org/] will identfy a query syntax hat uses JSON.stringify for the values of the attrubutes of the input object. The resulting string can also be passed to the portal, for example,
+
+		icgc.portal({cmd:"genes/ENSG00000141510",filters:{"mutation":{"functionalImpact":{is:["High"]}},"donor":{"projectId":{is:["MALY-DE"]}}, "tumourStageAtDiagnosis":{"is":["III"]}}})
+
+		note however that the portal search is only reacting to the filters. Accordingly, this command will generate the same result as
+
+		icgc.portal({filters:{"mutation":{"functionalImpact":{is:["High"]}},"donor":{"projectId":{is:["MALY-DE"]}}, "tumourStageAtDiagnosis":{"is":["III"]}}})
+
+		
+
+
+
--- icgc.js
+++ icgc.js
@@ -25,25 +25,24 @@ icgc.getScript = function (url,cb,er){ // load script / JSON
 	return s.id;
 }
 
-icgc.get = function(cmd,fun,parms){ // submit command cmd to icgc WebAPI and process it through callback function fun
-	if(!cmd){cmd='releases'}; // see http://www.icgcportal.org/public-portal/web_api.jsp for list of comands
+icgc.get = function(x,fun){ // submit command cmd to icgc WebAPI and process it through callback function fun
+	if(!x){x={cmd:'releases'}};
+	var cmd = x.cmd;
+	delete x.cmd;
 	if(!fun){fun=function(x){console.log(x)}};
 	if(!icgc.get.callbacks){icgc.get.callbacks={}};
 	if(!icgc.get.cache){icgc.get.cache={}};
 	var uid = this.uid('get');
-	if(!parms){
-		var Qparms="";
-	} else {
-		var Qparms="&";
-		Qparms+=icgc.parms(parms);
-	}
-	this.get.callbacks[uid]={ // maybe this can be used as a cache to avoid repeating calls
+	var Qparms="&"+icgc.parms(x);
+	this.get.callbacks[uid]={ // this can be used as a cache to avoid repeating calls
 		cmd:cmd,
 		fun:function(x){icgc.get.cache[cmd+Qparms]=x;return fun(x)},
 		t:Date.now()
 	}
 	if(!this.get.cache[cmd+Qparms]){ // this is not in the cache already
-		this.getScript('https://script.google.com/macros/s/AKfycbxjJBbr6dXXJGCLsJ-KZ_NacYsaNY-EsQv5HUMYvI_Fq13DNblq/exec?cmd='+cmd+'&callback=icgc.get.callbacks.'+uid+'.fun'+Qparms);
+		var url='https://script.google.com/macros/s/AKfycbxjJBbr6dXXJGCLsJ-KZ_NacYsaNY-EsQv5HUMYvI_Fq13DNblq/exec?cmd='+cmd+'&callback=icgc.get.callbacks.'+uid+'.fun'+Qparms;
+		console.log(url);
+		this.getScript(url);
 	} else {
 		icgc.get.callbacks[uid].fun(this.get.cache[cmd+Qparms]);
 	}
@@ -55,14 +54,57 @@ icgc.get = function(cmd,fun,parms){ // submit command cmd to icgc WebAPI and pro
 icgc.parms = function(x){ // converts JSON formated parameters into URL call query arguments
 	var y = '';
 	for (var f in x){
-		if(Array.isArray(x[f])){ // then turn them into a comma delimited string
-			var xx = x[f][0];
-			for (var i = 1 ; i<x[f].length ; i++){
-				xx += ',' + x[f][i];
-			}
-			x[f]=xx;
-		}
+		if(typeof(x[f])=="object"){x[f]=JSON.stringify(x[f])};
 		y+=f+'='+x[f]+'&';
 	}
 	return encodeURI(y.slice(0,y.length-1));
+}
+
+icgc.urlCall=function(x){ // generates the URL to ICGC's service, the same used by the Google AppScript proxy
+	if(!x){x={cmd:'releases'}};
+	var cmd = x.cmd;
+	delete x.cmd;
+	queryString = icgc.parms(x);
+	return "https://dcc.icgc.org/api/v1/"+cmd+"?"+queryString;
+}
+
+icgc.openUrl = function(url,op){
+	switch(op){
+	case undefined:
+		this.openUrl(url,2);
+		break; // move on
+	case 1: // open in a new window
+		window.open(url);
+		break;
+	case 2: // open in new iframe 
+		4
+		break;
+	default: // treating op as the parent DOM element
+		if(typeof(op)=="object"){var parentEl = op}
+		else { // op is the id of the parent DOM element
+			var parentEl = document.getElementById(op);
+			if(!parentEl){ // if not found create it and drop it in the body
+				var parentEl = document.createElement('div');
+				document.body.appendChild(parentEl);
+			}
+		}
+		var div = document.createElement('div');
+		div.id=icgc.uid();
+		//div.innerHTML='<a href="'+url+'" target=_blank>Open</a> in new window; <button style="color:red" onclick="icgc.removeEl(\''+div.id+'\')">Remove</button>';
+		div.innerHTML='<button onclick="window.open(\''+url+'\');" style="color:blue">Open</button> in new window; <button style="color:red" onclick="icgc.removeEl(\''+div.id+'\')">Remove</button>';
+		parentEl.appendChild(div);
+		var ifr = document.createElement('iframe');
+		div.appendChild(ifr);ifr.width="100%";ifr.height="100%";
+		ifr.src=url;
+	}
+	return false;
+}
+
+icgc.portal=function(x,id,s){ // open UI to icgc portal with filter, note s which can be "m" or "g" depending on the target representation being for genes or mutation
+	if(!s){var s = ""} else { s="/"+s }
+	var url = "https://dcc.icgc.org/search"+s+"?"+icgc.parms(x);
+	if(!!id){
+		icgc.openUrl(url,id);
+	}
+	return url;
 }
